e080de0c88297e754991d8b31891d70a00fbd62a
Amanda Saffer <asaffer@sosorgans.org>
2016-02-14 02:09:56 -0700

    Initial commit

---
+++ _books/sacred-bloodlines.md
@@ -0,0 +1,13 @@
+---
+layout: book
+title:  "Sacred Bloodlines"
+genre: "Young Adult Fantasy"
+cover-image: "sacred-bloodlines.jpg"
+series: "The Sacred Guardians Series"
+category: "latest"
+permalink: "/books/sacred-bloodlines"
+---
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lobortis convallis imperdiet. Nunc porta lorem a maximus tincidunt. Nullam vel hendrerit est. Praesent a efficitur enim. Maecenas condimentum ipsum a justo pretium consequat. Duis malesuada dolor a nisl ornare consectetur. Nulla a purus dictum, ornare odio eget, fringilla libero. 
+
+Mauris vel purus tortor. Etiam scelerisque, eros at tempus convallis, nisl leo mollis purus, vel pharetra urna est id metus. In eu diam non est ornare ultricies. Suspendisse ut augue dictum nulla sagittis viverra quis at neque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut sed diam et elit suscipit semper ac vel orci. Sed placerat efficitur nisi, ut sodales orci viverra eu.
---
+++ js/preload.js
@@ -0,0 +1,62 @@
+function preload(ele, imgName) {
+  var c = new Image(); // create img element for preload
+  var ele = ele;
+  
+  // when the image element is loaded, set the background image
+  c.onload = function(){
+    $(ele).attr("src", '/img/' + imgName);
+  }
+
+  // wait for image to load though
+  c.src = '/img/' + imgName;
+}
+
+function preloadBG(ele, imgName) {
+  var c = new Image(); // create img element for preload
+  var ele = ele;
+  
+  // when the image element is loaded, set the background image
+  c.onload = function(){
+    $(ele).css('background-image','url(/img/' + imgName + ')');
+  }
+
+  // wait for image to load though
+  c.src = '/img/' + imgName;
+}
+
+
+function cycleImages() {
+  var $title = $('.site-title h1.title');
+  var $active = $('.active');
+  var $next = ($active.next().length > 0) ? $active.next() : $('.book-covers:first');
+
+  var $lit = $('.lit');
+  var $nextlit = ($lit.next().length > 0) ? $lit.next() : $('.indicator:first');
+
+  $title.fadeOut(400,function(){
+    $title.html($next.attr("alt"));
+    $title.fadeIn(400);
+  });
+
+  $next.fadeIn(800);
+  $active.delay(100).fadeOut(500);
+  $active.removeClass('active');
+  $next.addClass('active');
+  $lit.removeClass('lit');
+  $nextlit.addClass('lit'); 
+}
+
+$(document).ready(function() {
+  preload('.book-covers.stubborn-love ', 'stubborn-love-books.png');
+  preload('.book-covers.sacred-guardians ', 'sacred-guardians-books.png');
+  preload('.book-covers.wash-me-away', 'wash-me-away-books.png');
+  preloadBG('.landing-hero', 'background.jpg');
+
+  // run every ??s
+  setInterval('cycleImages()', 7000);
+});
+
+document.querySelector( "#site-menu" ).addEventListener( "click", function() {
+  this.classList.toggle( "active" );
+  $('#expanded-menu').fadeToggle(350);
+});
